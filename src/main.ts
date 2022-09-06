b7cb47e417fda213b60bb4962b1ff89841a9276d
ricomasgu <ricard.comas.gomez@gmail.com>
2022-09-06 12:35:13 +0200

    Setting up the microservice. We are using the GRPC transport.

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
+
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'app',
+    protoPath: join(__dirname, '../src/app.proto'),
+  },
+};
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+  logger.log('Microservice is listening...');
+  await app.listen();
+}
+bootstrap();
